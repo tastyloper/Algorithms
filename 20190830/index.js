0e3eccc6f7f7d126710d5df87418a4ae36bd637e
tastyloper <estprocho@gmail.com>
2019-08-30 11:58:30 +0900

    20190830

---
+++ 20190830/index.js
@@ -0,0 +1,31 @@
+/**
+ * 58. Length of Last Word
+ *
+ * Given a string s consists of upper/lower-case alphabets
+ * and empty space characters ' ', return the length of last word in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ *
+ * Example:
+ * Input: 'Hello World'
+ * Output: 5
+ *
+ * https://leetcode.com/problems/length-of-last-word/
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = (s) => {
+  const split = s.split(' ').filter(n => n);
+  console.log(split);
+  return split[split.length - 1].length;
+};
+
+console.log(lengthOfLastWord('Hello World'));
+console.log(lengthOfLastWord('Length of Last Word'));
+console.log(lengthOfLastWord('a '));
+console.log(lengthOfLastWord('b   a    '));
